0a325a7d2cecd5000989384248778b1316cce24b
재민 <jjm0203311@naver.com>
2025-02-08 20:29:47 +0900

    도메인 수정

--- src/main/java/com/study/board/Config/Config.java
+++ src/main/java/com/study/board/Config/Config.java
@@ -14,7 +14,7 @@ public class Config {
     public CorsFilter corsFilter() {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("*"));
+        config.setAllowedOrigins(List.of("http://localhost:8080"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));
         config.setAllowedHeaders(List.of("*"));
         source.registerCorsConfiguration("/**", config);
